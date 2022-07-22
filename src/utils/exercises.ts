12879fc19ae6d6e601f36a348b476a3a0b44e95d
Dan McAtee <danielmcatee@me.com>
2022-07-22 15:53:45 +0200

    add exercises

---
+++ src/utils/exercises.ts
@@ -0,0 +1,96 @@
+/**
+ * Task description: Write a method that reverts input array
+ * Expected Result: [1, 2, 3] => [3, 2, 1]
+ * Task Complexity: 1 of 5
+ * @param {Array} array - Array of any elements
+ * @returns {Array}
+ */
+ export const reverse = (array: number[]) => {
+  throw new Error(`put your solution here ${array}`);
+};
+
+/**
+ * Task description: Write a method that clears array from all unnecessary elements, like false, undefined, empty strings, zero, null
+ * Expected Result: [0, 1, false, 2, undefined, '', 3, null] => [1, 2, 3]
+ * Task Complexity: 1 of 5
+ * @param {Array} array - An array of any elements
+ * @returns {Array}
+ */
+ export const compact = (array: any[]) => {
+  throw new Error(`put your solution here ${array}`);
+};
+
+/**
+ * Task description: Write a method that returns an array without listed values
+ * Expected Result: [1, 2, 3, 1, 2] without 1, 2 => [3]
+ * Task Complexity: 2 of 5
+ * @param {Array} array - Array of primitive data types
+ * @param {?} args list of values to remove
+ * @returns {Array}
+ */
+ export const without = (array: number[], ...args: number[]) => {
+  throw new Error(`put your solution here ${array} ${args}`);
+};
+
+
+
+/**
+ * Task description: Write a method that returns a duplicate-free array
+ * Expected Result: Duplicate-free array [1, 2, 3, 1, 2] => [1, 2, 3]
+ * Task Complexity: 2 of 5
+ * @param {Array<string | number>} array - Array of primitive data types
+ * @returns {Array}
+ */
+ export const unique = (array: number[]) => {
+  throw new Error(`put your solution here ${array}`);
+};
+
+
+
+/**
+ * Task description: Write a method that makes a shallow compare of two arrays and returns true if they are identical.
+ * Expected Result: ([1, 2, 3], [1, 2, 3]) => true
+ * Task Complexity: 2 of 5
+ * @param {Array} firstArray - Array of primitive data types
+ * @param {Array} secondArray - Array of primitive data types
+ * @returns {boolean}
+ */
+ export const isEqual = (firstArray: number[], secondArray: number[]) => {
+  throw new Error(`put your solution here ${firstArray} ${secondArray}`);
+};
+
+/**
+ * Task description: Write a method that turns a deep array into a plain array
+ * Expected Result: [1, 2, [3, 4, [5]]] => [1, 2, 3, 4, 5]
+ * Task complexity: 3 of 5
+ * @param {Array} array - A deep array
+ * @returns {Array}
+ */
+
+ export type NestedArray<T> = Array<T> | Array<NestedArray<T>>;
+ export const flatten = (array: NestedArray<number>) => {
+  throw new Error(`put your solution here ${array}`);
+};
+
+/**
+ * Task description: Write a method that splits an array into parts of determined size
+ * Expected Result: ([1, 2, 3, 4, 5], 2) => [[1, 2], [3, 4], [5]]
+ * Task complexity: 3 of 5
+ * @param {Array} array - An array of any elements
+ * @param {number} size - size of chunks
+ * @returns {Array}
+ */
+ export const chunk = (array: number[], size: number) => {
+  throw new Error(`put your solution here ${array} ${size}`);
+};
+
+/**
+ * Task description: Write a method that creates an array of unique values that are included in all given arrays
+ * Expected Result: ([1, 2], [2, 3]) => [2]
+ * Task complexity: 4 of 5
+ * @param {?} arrays - Arrays of primitive data types
+ * @returns {Array}
+ */
+ export const intersection = (...arrays: number[][]) => {
+  throw new Error(`put your solution here ${arrays}`);
+};
